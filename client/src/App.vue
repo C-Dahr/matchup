eac8295ffe015ac9c875ca4d80355b2ce05d121c
Cameron Dahr <CameronSDahr@gmail.com>
2020-01-20 14:00:55 -0400

    Added placeholder navbar as a component and added to App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="app">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <Navbar/>
     <router-view/>
   </div>
 </template>
 
+<script>
+
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'Login',
+  components: {
+    Navbar,
+  },
+};
+
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
