425e08224b13b876c002cf5eb28ef87c1f1dd3ca
Cameron Dahr <CameronSDahr@gmail.com>
2020-01-20 13:26:24 -0400

    Added navbar to login page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <img alt="Matchup logo" src="../src/assets/matchuplogo_outline.png">
-    <div id="nav">
-      <router-link to="/">Login</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -17,7 +12,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: #404040;
 }
 
 #nav {
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="login">
-    <h1>Login page</h1>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <!-- Brand -->
+      <a class="navbar-brand" href="#"></a>
+
+      <!-- Links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 2</a>
+        </li>
+
+        <!-- Dropdown -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+            Dropdown link
+          </a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Link 1</a>
+            <a class="dropdown-item" href="#">Link 2</a>
+            <a class="dropdown-item" href="#">Link 3</a>
+          </div>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
+
+<style>
+
+.navbar-brand {
+    position: relative;
+    background: url(../assets/matchuplogo_outline.png);
+    width: 170px;
+    left: 15px;
+    background-size: contain;
+}
+
+</style>
