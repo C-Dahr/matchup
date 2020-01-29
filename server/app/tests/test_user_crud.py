4685015c05826b7df3dd4a6680630ca1bb2618a2
dmccardle <danny.mccardle@unb.ca>
2020-01-29 19:48:53 -0400

    Created file for running User CRUD tests

---
+++ server/app/tests/test_user_crud.py
@@ -0,0 +1,19 @@
+import unittest
+from flask_testing import TestCase
+
+class TestUserCrudOperations(TestCase):
+  def create_user(self):
+
+
+  def delete_user(self):
+
+
+  def update_user(self):
+
+
+  def get_user_by_id(self):
+
+
+
+if __name__ == '__main__':
+  unittest.main()
