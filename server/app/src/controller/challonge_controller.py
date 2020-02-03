4991b7fb1b17d3bb2475dde7d3502c167bfff975
Tayler Harvey <tharvey2@unb.ca>
2020-02-02 21:02:54 -0400

    Set credentials using json input (no auth token). Includes try statement in case of invalid credentials

--- server/app/src/controller/challonge_controller.py
+++ server/app/src/controller/challonge_controller.py
@@ -13,20 +13,14 @@ api = Namespace('challonge', description='challonge related functionality')
 class BracketController(Resource):
     @api.doc('get brackets')
     def get(self):
-        # check header for auth token
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            api.abort(404, 'No user signed in.')
-        # decode token and get user
-        data = jwt.decode(token, 'my_secret_key')
-        current_user = User.query.filter_by(id=data['id']).first()
-        if not current_user:
-            api.abort(404, 'Invalid token.')
+        username = request.json['username']
+        api_key = request.json['api_key']
 
-        # set the credentials for interfacing with challonge
-        challonge.set_credentials(current_user.username, current_user.api_key)
-
-        # index returns a list of the user's tournaments
-        tournaments = challonge.tournaments.index()
-        return jsonify({'tournaments' : tournaments})
+        try:
+            # set the credentials for interfacing with challonge
+            challonge.set_credentials(username, api_key)
+            # index returns a list of the user's tournaments
+            tournaments = challonge.tournaments.index()
+            return jsonify({'tournaments' : tournaments})
+        except Exception as e:
+            api.abort(401, 'Invalid credentials.')
