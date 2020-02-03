8540cc72a19ddf09695b5e315c93ba72d0ce90a9
Tayler Harvey <tharvey2@unb.ca>
2020-02-03 11:48:56 -0400

    Sets challonge username from json input and api_key from auth token

--- server/app/src/controller/challonge_controller.py
+++ server/app/src/controller/challonge_controller.py
@@ -14,13 +14,23 @@ class BracketController(Resource):
     @api.doc('get brackets')
     def get(self):
         username = request.json['username']
-        api_key = request.json['api_key']
+        # check header for auth token
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            api.abort(404, 'No user signed in.')
+        # decode token and get user
+        data = jwt.decode(token, 'my_secret_key')
+        current_user = User.query.filter_by(id=data['id']).first()
+        if not current_user:
+            api.abort(404, 'Invalid token.')
 
         try:
             # set the credentials for interfacing with challonge
-            challonge.set_credentials(username, api_key)
-            # index returns a list of the user's tournaments
-            tournaments = challonge.tournaments.index()
-            return jsonify({'tournaments' : tournaments})
+            challonge.set_credentials(username, current_user.api_key)
         except Exception as e:
             api.abort(401, 'Invalid credentials.')
+        
+        # index returns a list of the user's tournaments
+        tournaments = challonge.tournaments.index()
+        return jsonify({'tournaments' : tournaments})
