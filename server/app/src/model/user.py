9649bac74d49eb5eb8612a181f2ab07b10282f07
dmccardle <danny.mccardle@unb.ca>
2020-02-04 17:36:41 -0400

    Added `challonge_username` field to User model

--- server/app/src/model/user.py
+++ server/app/src/model/user.py
@@ -5,15 +5,17 @@ class User(db.Model):
   username = db.Column(db.String(50), unique=True)
   password = db.Column(db.String(100))
   email = db.Column(db.String(100), unique=True)
+  challonge_username = db.Column(db.String(100), unique=True)
   api_key = db.Column(db.String(100))
 
-  def __init__(self, username, password, email, api_key):
+  def __init__(self, username, password, email, challonge_username, api_key):
     self.username = username
     self.password = password
     self.email = email
+    self.challonge_username = challonge_username
     self.api_key = api_key
 
 
 class UserSchema(ma.Schema):
   class Meta:
-    fields = ('id', 'username', 'email', 'api_key')
+    fields = ('id', 'username', 'email', 'challonge_username', 'api_key')
