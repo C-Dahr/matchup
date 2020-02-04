764ecc04a8509488e505c732cfa075fabc6c0191
Tayler Harvey <tharvey2@unb.ca>
2020-02-04 18:28:23 -0400

    Change 4 space indents to 2 space indents and challonge credentials are now both set using token info

--- server/app/src/controller/auth_controller.py
+++ server/app/src/controller/auth_controller.py
@@ -13,20 +13,20 @@ api = Namespace('auth', description='auth related functionality')
 
 @api.route('')
 class LoginController(Resource):
-    @api.doc('user login')
-    def post(self):
-        auth = request.authorization
-        username = auth.username
-        password = auth.password
-        user = User.query.filter_by(username=username).first()
-        if user:
-            if check_password_hash(user.password, password):
-                auth_token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, key)
-                if auth_token:
-                    return jsonify({'token' : auth_token.decode('UTF-8')})
-                else:
-                    api.abort(404, 'Token could not be generated.')
-            else:
-                api.abort(404, 'Password is incorrect.')
+  @api.doc('user login')
+  def post(self):
+    auth = request.authorization
+    username = auth.username
+    password = auth.password
+    user = User.query.filter_by(username=username).first()
+    if user:
+      if check_password_hash(user.password, password):
+        auth_token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, key)
+        if auth_token:
+          return jsonify({'token' : auth_token.decode('UTF-8')})
         else:
-            api.abort(404, 'User does not exist.')
+          api.abort(404, 'Token could not be generated.')
+      else:
+          api.abort(404, 'Password is incorrect.')
+    else:
+      api.abort(404, 'User does not exist.')
--- server/app/src/controller/challonge_controller.py
+++ server/app/src/controller/challonge_controller.py
@@ -12,26 +12,25 @@ api = Namespace('challonge', description='challonge related functionality')
 
 @api.route('')
 class BracketController(Resource):
-    @api.doc('get brackets')
-    def get(self):
-        username = request.json['username']
-        # check header for auth token
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            api.abort(401, 'No user signed in.')
-        # decode token and get user
-        data = jwt.decode(token, key)
-        current_user = User.query.filter_by(id=data['id']).first()
-        if not current_user:
-            api.abort(404, 'Invalid token.')
+  @api.doc('get brackets')
+  def get(self):
+    # check header for auth token
+    if 'x-access-token' in request.headers:
+        token = request.headers['x-access-token']
+    if not token:
+        api.abort(401, 'No user signed in.')
+    # decode token and get user
+    data = jwt.decode(token, key)
+    current_user = User.query.filter_by(id=data['id']).first()
+    if not current_user:
+        api.abort(404, 'Invalid token.')
 
-        try:
-            # set the credentials for interfacing with challonge
-            challonge.set_credentials(username, current_user.api_key)
-        except Exception as e:
-            api.abort(401, 'Invalid credentials.')
-        
-        # index returns a list of the user's tournaments
-        tournaments = challonge.tournaments.index()
-        return jsonify({'tournaments' : tournaments})
+    try:
+        # set the credentials for interfacing with challonge
+        challonge.set_credentials(current_user.challonge_username, current_user.api_key)
+    except Exception as e:
+        api.abort(401, 'Invalid credentials.')
+    
+    # index returns a list of the user's tournaments
+    tournaments = challonge.tournaments.index()
+    return jsonify({'tournaments' : tournaments})
