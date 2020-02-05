d6b2ffb52466dea289888dc8534bde06e07e0d49
dmccardle <danny.mccardle@unb.ca>
2020-02-05 14:53:55 -0400

    Added Cross Origin support for backend

--- server/app/src/__init__.py
+++ server/app/src/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 from .config import config_by_name
 
@@ -12,5 +13,6 @@ def create_app(config_name):
   app.config.from_object(config_by_name[config_name])
   db.init_app(app)
   ma.init_app(app)
+  CORS(app)
 
   return app
