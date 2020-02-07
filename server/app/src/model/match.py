aef3052014b70b908c0c0eee9f9ae58af3bbc911
dmccardle <danny.mccardle@unb.ca>
2020-02-07 15:18:12 -0400

    Added class for Match objects

---
+++ server/app/src/model/match.py
@@ -0,0 +1,9 @@
+class Match:
+  def __init__(self, id, player1_id, player2_id, round_in_bracket, tournament_id, priority, game_name):
+    self.id = id
+    self.player1_id = player1_id
+    self.player2_id = player2_id
+    self.round_in_bracket = round_in_bracket
+    self.tournament_id = tournament_id
+    self.priority = priority
+    self.game_name = game_name
