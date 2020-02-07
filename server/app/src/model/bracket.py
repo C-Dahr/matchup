aa7136b43048c6b305202d7d6d680684349b3a79
dmccardle <danny.mccardle@unb.ca>
2020-02-07 15:10:29 -0400

    Added class for Bracket objects

---
+++ server/app/src/model/bracket.py
@@ -0,0 +1,6 @@
+class Bracket:
+  def __init__(self, id, game_name, number_of_setups, number_of_players):
+    self.id = id
+    self.game_name = game_name
+    self.number_of_setups = number_of_setups
+    self.number_of_players = number_of_players
