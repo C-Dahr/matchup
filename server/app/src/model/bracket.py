793ccd30c96562ef75a51a7601fb81432a9d573e
dmccardle <danny.mccardle@unb.ca>
2020-02-08 14:31:12 -0400

    Added "BracketSource" enum to brackets to represent what platform the bracket is being run on (for if it is expanded in the future to support smashgg)

--- server/app/src/model/bracket.py
+++ server/app/src/model/bracket.py
@@ -1,6 +1,12 @@
+from enum import Enum
+class BracketSource(Enum):
+  CHALLONGE = 'challonge'
+  SMASHGG = 'smashgg'
+
 class Bracket:
-  def __init__(self, id, game_name, number_of_setups, number_of_players):
+  def __init__(self, id, source, game_name, number_of_setups, number_of_players):
     self.id = id
+    self.source = BracketSource(source)
     self.game_name = game_name
     self.number_of_setups = number_of_setups
     self.number_of_players = number_of_players
