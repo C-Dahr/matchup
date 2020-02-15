75bc015cb5c54dfdafbbf8179dc7bc506d2d6afc
dmccardle <danny.mccardle@unb.ca>
2020-02-15 19:26:09 -0400

    Updated fields on edit profile to match functionality on sign up

--- client/src/views/EditProfile.vue
+++ client/src/views/EditProfile.vue
@@ -3,36 +3,56 @@
     <div class="form-title">
       <p>Edit Profile</p>
     </div>
+    <div class="form-error-list" v-if="errors.length">
+      <ul class="form-error">
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+    </div>
     <div class="d-flex justify-content-center">
-      <form @submit="onSubmit"  method="put" class="account-form">
+      <form @submit="onSubmit"  method="post" class="account-form">
             <div class="form-group d-flex justify-content-left">
-              <label class="form-label">Username:</label>
+              <label class="form-label">Username <span class="required-star">*</span></label>
               <input class="form-control" type="text"
-              name="username" required placeholder="Enter Username"/>
+              name="username" v-model="editProfileForm.username"
+              required placeholder="Enter Username"/>
               <span class="Error"></span>
             </div>
             <div class="form-group d-flex justify-content-left">
-              <label class="form-label">Email:</label>
+              <label class="form-label">Email <span class="required-star">*</span></label>
               <input class="form-control" type="text"
-              name="email" required placeholder="Enter Email"/>
+              name="email" v-model="editProfileForm.email"
+              required placeholder="Enter Email"/>
               <span class="Error"></span>
             </div>
             <div class="form-group d-flex justify-content-left">
-              <label class="form-label">Password:</label>
+              <label class="form-label">Password <span class="required-star">*</span></label>
               <input class="form-control" type="password"
-                 name="password" required placeholder="Enter Password"/>
+                 name="password" v-model="editProfileForm.password"
+                 required placeholder="Enter Password"/>
                 <span class="Error"></span>
             </div>
             <div class="form-group d-flex justify-content-left">
-              <label class="form-label">Confirm Password:</label>
+              <label class="form-label">
+                Confirm Password
+                <span class="required-star">*</span>
+              </label>
               <input class="form-control" type="password"
-                 name="passwordConfirm" required placeholder="Confirm Password"/>
+                 name="passwordConfirm" v-model="editProfileForm.confirm_password"
+                 required placeholder="Confirm Password"/>
                 <span class="Error"></span>
             </div>
             <div class="form-group d-flex justify-content-left">
-              <label class="form-label">Challonge API Key:</label>
+              <label class="form-label">Challonge Username</label>
+              <input class="form-control" type="text"
+              name="challonge_username" v-model="editProfileForm.challonge_username"
+              placeholder="Enter Challonge Username"/>
+              <span class="Error"></span>
+            </div>
+            <div class="form-group d-flex justify-content-left">
+              <label class="form-label">Challonge API Key</label>
               <input class="form-control" type="password"
-                 name="api_key" required placeholder="Enter Challonge API Key"/>
+                 name="api_key" v-model="editProfileForm.api_key"
+                 placeholder="Enter Challonge API Key"/>
                 <span class="Error"></span>
             </div>
             <h4>Generate or find an existing API key for Challonge <b-link href="https://challonge.com/settings/developer" target="_blank">here</b-link></h4>
@@ -51,10 +71,13 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      errors: [],
       editProfileForm: {
         username: '',
         email: '',
         password: '',
+        confirm_password: '',
+        challonge_username: '',
         api_key: '',
       },
     };
@@ -63,25 +86,33 @@ export default {
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
-      const payload = {
-        username: this.editProfileForm.username,
-        email: this.editProfileForm.email,
-        password: this.editProfileForm.password,
-        api_key: this.editProfileForm.api_key,
-      };
-      this.updateUser(payload);
+      this.errors = [];
+      if (this.editProfileForm.password !== this.editProfileForm.confirm_password) {
+        this.editProfileForm.password = '';
+        this.editProfileForm.confirm_password = '';
+        this.errors.push('Passwords do not match');
+      } else {
+        const payload = {
+          username: this.editProfileForm.username,
+          email: this.editProfileForm.email,
+          password: this.editProfileForm.password,
+          challonge_username: this.editProfileForm.challonge_username,
+          api_key: this.editProfileForm.api_key,
+        };
+        this.createUser(payload);
+      }
     },
     updateUser(payload) {
-      const path = 'http://localhost:5000/user/$<id>';
+      const path = 'http://localhost:5000/user';
       axios.put(path, payload)
         .then(() => {
           this.$router.push('/');
         })
         .catch((error) => {
-          // eslint-disable-next-line
-          console.log(error);
+          if (error.response.status === 409) {
+            this.errors.push('Username already exists');
+          }
         });
-      // this.$router.push('/');
     },
   },
 };
