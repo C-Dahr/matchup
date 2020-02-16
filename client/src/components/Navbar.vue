1d372a5b6669976196667fdda1bc3790b8fc28ad
Cameron Dahr <CameronSDahr@gmail.com>
2020-02-16 00:15:50 -0400

    Added check for login on welcome msg

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,23 +1,23 @@
 <template>
     <nav class="navbar navbar-expand-sm" id="navbar-main">
-        <!-- Brand -->
-        <a class="navbar-brand" href="/"></a>
+      <!-- Brand -->
+      <a class="navbar-brand" href="/"></a>
 
-        <!-- Links -->
-        <ul class="navbar-nav ml-auto d-lg-flex align-items-center">
-            <li class="nav-item">
-              <a class="nav-link" v-if="!isLoggedIn" href="/login">Login</a>
-            </li>
-            <li class="nav-item">
-              <div id="welcome-msg">Welcome, {{ username }}</div>
-              <a class="nav-link" @click="logout" v-if="isLoggedIn" href="/login">Sign out</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="profile" v-if="isLoggedIn">
-                <font-awesome-icon icon="user-circle" size="3x"/>
-              </a>
-            </li>
-        </ul>
+      <!-- Links -->
+      <ul class="navbar-nav ml-auto d-lg-flex align-items-center">
+        <li class="nav-item">
+          <a class="nav-link" v-if="!isLoggedIn" href="/login">Login</a>
+        </li>
+        <li class="nav-item">
+          <div id="welcome-msg" v-if="isLoggedIn">Welcome, {{ username }}</div>
+          <a class="nav-link" @click="logout" v-if="isLoggedIn" href="/login">Sign out</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="profile" v-if="isLoggedIn">
+            <font-awesome-icon icon="user-circle" size="3x"/>
+          </a>
+        </li>
+      </ul>
     </nav>
 </template>
 
@@ -56,18 +56,15 @@ export default {
     background-color: #0066FF;
 }
 
-.nav-link {
+.navbar .nav-link {
     color: white;
+    padding-top: 0;
+    padding-bottom: 0;
+    text-align: right;
 }
 
 .navbar-nav {
   padding-right: 1em;
-}
-
-.navbar-nav .nav-link {
-  padding-top: 0;
-  padding-bottom: 0;
-  text-align: right;
 }
 
 .nav-item {
