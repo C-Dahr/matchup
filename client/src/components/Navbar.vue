913c79ff2b83ba92c37a8719c2bff8e914912595
Cameron Dahr <CameronSDahr@gmail.com>
2020-02-15 22:23:10 -0400

    - Fixed upper right corner of navbar to display icon/name/signout message in a nicer looking way - Clicking profile icon now routes to '/profile'

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -4,17 +4,17 @@
         <a class="navbar-brand" href="/"></a>
 
         <!-- Links -->
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav ml-auto d-lg-flex align-items-center">
             <li class="nav-item">
               <a class="nav-link" v-if="!isLoggedIn" href="/login">Login</a>
             </li>
-            <li class="nav-item ">
-              <a class="nav-link" @click="logout" v-if="isLoggedIn" href="/login">Logout</a>
+            <li class="nav-item">
+              <div id="welcome-msg">Welcome, {{ username }}</div>
+              <a class="nav-link" @click="logout" v-if="isLoggedIn" href="/login">Sign out</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" v-if="isLoggedIn">
-                <font-awesome-icon icon="user-circle" size="2x"/>
-                <div>{{ username }}</div>
+              <a class="nav-link" href="profile" v-if="isLoggedIn">
+                <font-awesome-icon icon="user-circle" size="3x"/>
               </a>
             </li>
         </ul>
@@ -49,6 +49,7 @@ export default {
     width: 170px;
     left: 15px;
     background-size: contain;
+    padding: 0;
 }
 
 #navbar-main {
@@ -63,4 +64,17 @@ export default {
   padding-right: 1em;
 }
 
+.navbar-nav .nav-link {
+  padding-top: 0;
+  padding-bottom: 0;
+  text-align: right;
+}
+
+.nav-item {
+  padding-right: 1em;
+}
+
+#welcome-msg {
+  font-size: 1.5em;
+}
 </style>
