7b31a176039392e4c1b3eacb2ac0ae6cc225d675
Cameron Dahr <CameronSDahr@gmail.com>
2020-02-19 15:05:17 -0400

    Updated navbar to ultilize @media screen width to hide welcome message on smaller screens and cleaned up the navbar code

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,22 +1,19 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" id="navbar-main">
-      <b-navbar-brand  href="/"></b-navbar-brand>
+  <b-navbar id="navbar-main">
+    <b-navbar-brand  href="/"></b-navbar-brand>
       <b-navbar-nav v-if="isLoggedIn" class="ml-auto" id="navbar-right">
-        <b-collapse id="nav-collapse" is-nav>
-          <b-nav-text id="welcome-msg">Welcome, {{ username }}</b-nav-text>
-        </b-collapse>
-        <b-nav-item-dropdown right>
+        <b-nav-text id="welcome-msg">Welcome, {{ username }}</b-nav-text>
+        <b-nav-item-dropdown no-caret right>
           <template v-slot:button-content>
             <font-awesome-icon icon="user-circle" size="3x" style="color: white;"/>
           </template>
-          <b-dropdown-item href="/editprofile">Profile</b-dropdown-item>
-          <b-dropdown-item @click="logout" href="/login">Sign Out</b-dropdown-item>
+            <b-dropdown-item href="/editprofile">Profile</b-dropdown-item>
+            <b-dropdown-item @click="logout" href="/login">Sign Out</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
-    </b-navbar>
-  </div>
+  </b-navbar>
 </template>
+
 
 <script>
 
@@ -53,17 +50,13 @@ export default {
   background-color: #0066FF;
 }
 
-#welcome-msg {
-  color: white;
-  font-size: 1.5em;
-}
-
 #navbar-right {
   padding-right: 2em;
 }
 
-.dropdown-toggle::after {
-  content: none !important;
+#welcome-msg {
+  color: white;
+  font-size: 1.5em;
 }
 
 .navbar-collapse.collapse {
@@ -74,6 +67,12 @@ export default {
   border-top-left-radius: 5px !important;
   border-top-right-radius: 5px !important;
   max-width: 50px;
+}
+
+@media only screen and (max-width: 600px) {
+  #welcome-msg {
+    display: none;
+  }
 }
 
 </style>
