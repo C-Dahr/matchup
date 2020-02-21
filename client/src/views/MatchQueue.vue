08829f1cb2ef5e8fbc9e84d0c92587c3b2ef68a4
Zachary Ferguson <zacharyferg98@gmail.com>
2020-02-20 22:22:40 -0400

    Match Queue cards and elements created. Styling still needed

--- client/src/views/MatchQueue.vue
+++ client/src/views/MatchQueue.vue
@@ -2,26 +2,47 @@
   <div class="match-queue">
     <div class="form-title">
       <p>Event Name</p>
+      <input class="btn btn-danger btn-lg" type="submit"
+                      value="End Event"/>
     </div>
-    <b-container class="bv-example-row card-deck">
-        <b-row class="card border-light">
+    <b-container class="card-deck">
+      <div v-for="matchData in matchData" v-bind:key="matchData">
+        <b-row align-h="center" class="card border-light">
             <div class="card-body">
-                <h3 class="card-title">Bracket Two</h3>
+                <h3 class="card-title">{{ matchData.player1}} vs. {{matchData.player2}}</h3>
+                <h4 class="card-title">{{ matchData.game}}</h4>
             </div>
         </b-row>
-        <b-row>
+        <b-row align-h="center">
             <input class="btn btn-success btn-lg in-progress-submit" type="submit"
                  value="Mark In Progress"/>
         </b-row>
+      </div>
     </b-container>
   </div>
 </template>
 
+<script>
+// import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      errors: [],
+      maxVisibleMatches: 5,
+      matchData: [
+        { player1: 'ZachAtk', player2: 'CamBlam', game: 'Game 1' },
+        { player1: 'DannyGranE', player2: 'TaylerMailr', game: 'Game 1' },
+      ],
+    };
+  },
+};
+</script>
 
 <style scoped>
 .card {
-    background-color: #0066FF !important;
-    width: 30rem;
+  background-color: #0066FF !important;
+  width: 50rem;
 }
 .card-title{
     font-weight: bold;
@@ -31,6 +52,13 @@
   min-width: 500px;
   display: flex;
   justify-content: center;
+}
+.in-progress-submit {
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+.btn-danger {
+  height: 60px;
 }
 
 </style>
