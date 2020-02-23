2176efcdb3e1c21238aef21f8a5ac7311c9d9f57
dmccardle <danny.mccardle@unb.ca>
2020-02-23 15:52:03 -0400

    Added route for returning a list of all matches available on event

--- server/app/src/controller/challonge_controller.py
+++ server/app/src/controller/challonge_controller.py
@@ -1,6 +1,6 @@
 from .. import db, ma
-from ..model.user import UserSchema
-from ..model.user import User
+from ..model.user import UserSchema, User
+from ..model.event import Event
 from flask import request, jsonify
 from flask_restplus import Resource, Namespace
 from app.src.controller import get_user_from_auth_header
@@ -11,7 +11,7 @@ import challonge
 
 api = Namespace('challonge', description='challonge related functionality')
 
-@api.route('')
+@api.route('/bracket')
 class BracketController(Resource):
   @api.doc('get brackets')
   def get(self):
@@ -25,4 +25,18 @@ class BracketController(Resource):
     except HTTPError as e:
       api.abort(401, 'Invalid credentials.')
 
-
+@api.route('/match/<event_name>')
+class MatchController(Resource):
+  @api.doc('get matches for an event')
+  def get(self, event_name):
+    current_user = get_user_from_auth_header(request, api)
+    event = Event.query.get({'event_name': event_name, 'user_id': current_user.id})
+    try:
+      challonge.set_credentials(current_user.challonge_username, xor_crypt_string(current_user.api_key, decode=True))
+      brackets = event.brackets
+      bracket_1_matches = challonge.matches.index(tournament=brackets[0]['id'], state='open')
+      bracket_2_matches = challonge.matches.index(tournament=brackets[1]['id'], state='open')
+      matches = bracket_1_matches + bracket_2_matches
+      return jsonify({'matches' : matches})
+    except HTTPError as e:
+      api.abort(401, 'Invalid credentials.')
