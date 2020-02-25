35ca54d1eb39294a3313672822518db67b98a506
Cameron Dahr <CameronSDahr@gmail.com>
2020-02-25 18:03:28 -0400

    Fixed more UI elements on the match queue and added a grear icon that leads to an editEvent page

--- client/src/views/MatchQueue.vue
+++ client/src/views/MatchQueue.vue
@@ -1,34 +1,33 @@
 <template>
   <div class="match-queue">
     <b-container>
-      <b-row align-h="between">
-        <b-col sm="2">
-        </b-col>
+      <b-row id="titleCard" align-h="between" align-v="center">
+        <b-col sm="2"/>
         <b-col sm="8">
           <div class="form-title">
-            <p>Event Name</p>
+            <p>Event Name <a href="editEvent"><font-awesome-icon icon="cog" size=""/></a></p>
           </div>
         </b-col>
         <b-col sm="2">
-          <input class="btn btn-danger btn-lg" type="submit" value="End Event"/>
+          <input class="btn btn-danger" type="submit" value="End Event"/>
         </b-col>
       </b-row>
     </b-container>
     <b-container>
-        <div v-for="matchData in matchData" class="match" v-bind:key="matchData">
-          <b-row align-h="center">
-            <b-card border-variant="light">
-              <div class="card-body">
-                  <h3 class="card-title">{{ matchData.player1}} vs. {{matchData.player2}}</h3>
-                  <h4 class="card-title">{{ matchData.game}}</h4>
-              </div>
-            </b-card>
-          </b-row>
-          <b-row align-h="center">
-              <input class="btn btn-success btn-lg in-progress-submit" type="submit"
-                  value="Mark In Progress"/>
-          </b-row>
-        </div>
+      <div v-for="matchData in matchData" class="match" v-bind:key="matchData">
+        <b-row align-h="center">
+          <b-card border-variant="light">
+            <div class="card-body">
+              <h3 class="card-title">{{ matchData.player1}} vs. {{matchData.player2}}</h3>
+              <h4 class="card-title">{{ matchData.game}}</h4>
+            </div>
+          </b-card>
+        </b-row>
+        <b-row align-h="center">
+          <input class="btn btn-success btn-lg in-progress-submit" type="submit"
+          value="Mark In Progress"/>
+        </b-row>
+      </div>
     </b-container>
   </div>
 </template>
@@ -52,32 +51,29 @@ export default {
 
 <style scoped>
 .card {
-  background-color: #0066FF !important;
+  background-color: #0066FF;
   width: 50rem;
   margin: 0;
 }
-.col {
-  padding: 0;
-}
 .card-title{
     font-weight: bold;
-}
-.card-deck {
-  width: 60%;
-  min-width: 500px;
-  display: flex;
-  justify-content: center;
 }
 .in-progress-submit {
   margin-top: 10px;
   margin-bottom: 20px;
 }
-.btn-danger {
-  height: 60px;
-}
+
 @media only screen and (max-width: 600px) {
-  .match {
-    width: 95%;
+  .form-title{
+    padding-bottom: 0;
+  }
+
+  #titleCard{
+    padding-bottom: 1.5em;
+  }
+
+  .card {
+    width: 25rem;
   }
 }
 
