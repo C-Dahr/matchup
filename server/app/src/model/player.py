351d5137fee6658b519c8e57fc7b5b7d74d33717
Tayler Harvey <tharvey2@unb.ca>
2020-02-28 15:33:58 -0400

    The calm before the storm - added proposed tables before trying something else :)

--- server/app/src/model/player.py
+++ server/app/src/model/player.py
@@ -4,22 +4,7 @@ from .tables import bracket_players
 
 class Player(db.Model):
   id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-  challonge_id = db.Column(db.Integer, nullable=False)
-  name = db.Column(db.String(100), nullable=False)
-  seeds = db.Column(db.Integer, nullable=False)
   brackets = db.relationship("Bracket", secondary=bracket_players, back_populates='players')
-
-  def __init__(self, c_id, name, seeds):
-    self.challonge_id = c_id
-    self.name = name
-    self.seeds = seeds
-
-  # compares the player_id passed in against all ids for this player
-  def is_player(self, player_id):
-    for bracket_id in self.bracket_ids:
-      if player_id == bracket_id:
-        return True
-    return False
 
 class PlayerSchema(ma.Schema):
   class Meta:
--- server/app/src/model/tables.py
+++ server/app/src/model/tables.py
@@ -3,7 +3,19 @@ from .. import db, ma
 from flask import jsonify
 
 # helper tables file
+
 bracket_players = db.Table('bracket_players',
-    db.Column('player_id', db.Integer, db.ForeignKey('player.id'), primary_key=True, nullable=False),
-    db.Column('bracket_id', db.Integer, db.ForeignKey('bracket.id'), primary_key=True, nullable=False)
+  db.Column('player_id', db.Integer, db.ForeignKey('player.id'), primary_key=True, nullable=False),
+  db.Column('bracket_id', db.Integer, db.ForeignKey('bracket.id'), primary_key=True, nullable=False)
+)
+
+player_names = db.Table('player_names',
+  player_id = db.Column(db.Integer, db.ForeignKey('player.id'), primary_key=True, nullable=False)
+  bracket_id = db.Column(db.Integer, db.ForeignKey('bracket.id'), primary_key=True, nullable=False)
+  name = db.Column(db.String(100), nullable=False)
+)
+
+player_challonge_ids = db.Table('player_challonge_ids'
+  player_id = db.Column(db.Integer, db.ForeignKey('player.id'), primary_key=True, nullable=False)
+  challonge_id = db.Column(db.Integer, primary_key=True, nullable=False)
 )
