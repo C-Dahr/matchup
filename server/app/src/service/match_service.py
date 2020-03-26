6cb9201a0527b03d1d926162e8e284f28c92c86d
dmccardle <danny.mccardle@unb.ca>
2020-03-26 16:47:01 -0300

    Added priority parameter for the round the match is in relative to the rest of the matches

--- server/app/src/service/match_service.py
+++ server/app/src/service/match_service.py
@@ -2,21 +2,24 @@ import challonge
 from ..model.player import Player, PlayerSchema
 from ..model.bracket import Bracket, BracketSchema
 from ..model.tables import BracketPlayers, ChallongePlayer
+import statistics
 
 player_schema = PlayerSchema()
 bracket_schema = BracketSchema()
 
+player_overlap_priority_constant = 5
+round_priority_constant = 2
+
 def determine_priority_for_matches(matches_not_in_progress, bracket):
+  average_round = get_mean_match_round(matches_not_in_progress)
   for match in matches_not_in_progress:
     player1_data, player1 = build_player_data(match['player1_id'], bracket)
     player2_data, player2 = build_player_data(match['player2_id'], bracket)
 
-    # TODO: determine average round of this bracket
-
     match['player1'] = player1_data
     match['player2'] = player2_data
     match['bracket'] = bracket_schema.jsonify(bracket).json
-    match['priority'] = calculate_match_priority(match, player1, player2)
+    match['priority'] = calculate_match_priority(match, player1, player2, average_round)
   
   return matches_not_in_progress
 
@@ -29,13 +32,31 @@ def build_player_data(challonge_player_id, bracket):
   player_data['name'] = bracket_player.name
   return player_data, player
 
-def calculate_match_priority(match, player1, player2):
+def get_mean_match_round(matches):
+  rounds = []
+  for match in matches:
+    rounds.append(match['round'])
+  if len(rounds) != 0:
+    return statistics.mean(rounds)
+  else:
+    -1
+
+def calculate_match_priority(match, player1, player2, average_round):
   match_priority =  get_player_priority(player1) + get_player_priority(player2)
+  match_priority += get_bracket_priority(match)
+  match_priority += get_round_priority(match['round'], average_round)
   return match_priority
 
 def get_player_priority(player):
   # 1 bracket: priorty = 0, 2 brackets: priority = 5
-  return (len(player.brackets) - 1) * 5
+  return (len(player.brackets) - 1) * player_overlap_priority_constant
+
+def get_bracket_priority(match):
+  return 0
+
+def get_round_priority(match_round, average_round):
+  scale_factor = average_round / match_round
+  return scale_factor * round_priority_constant
 
 def get_highest_priority_matches(matches_sorted_by_priority, bracket_setups):
   matches_called = []
