a84c406d2831b93f2c44c65a1021fd1c94a7a866
Cameron Dahr <CameronSDahr@gmail.com>
2020-03-28 21:10:58 -0300

    Updated review/merge players to use drop down selectors instead of the checkboxes

--- client/src/views/ReviewPlayers.vue
+++ client/src/views/ReviewPlayers.vue
@@ -2,59 +2,74 @@
   <div class="review-players">
     <b-container>
       <div class="form-title">
-        <p>Review Players</p>
+        Review Players
+        <input class="btn btn-primary btn-lg btn-next"
+        onclick="onSubmit()" type="submit" value="Next"/>
       </div>
       <b-row align-h="between" align-v="center">
+          <b-col sm-4>
+              <div class="form-group d-flex justify-content-center">
+                <label class="form-label">Melee Player</label>
+                <model-select :options="melee"
+                    v-model="selectedMelee"
+                    placeholder="Select Player">
+                </model-select>
+            </div>
+          </b-col>
+          <b-col sm-4>
+              <div class="form-group d-flex justify-content-center">
+                <label class="form-label">Ultimate Player</label>
+                <model-select :options="ultimate"
+                    v-model="selectedUltimate"
+                    placeholder="Select Player">
+                </model-select>
+            </div>
+          </b-col>
+          <b-col sm-4>
+              <div class="form-group d-flex justify-content-center">
+                  <input class="btn btn-primary btn-lg merge-form-submit"
+                  type="submit" value="Merge"/>
+              </div>
+          </b-col>
+      </b-row>
+      <b-row align-h="between" align-v="center">
         <b-col sm-4>
-          <h2>Melee</h2>
+          <h2 class="review-header">Melee</h2>
         </b-col>
         <b-col sm-4>
-          <h2>Ultimate</h2>
+          <h2 class="review-header">Ultimate</h2>
         </b-col>
         <b-col sm-4>
-          <h2>Both</h2>
+          <h2 class="review-header">Both</h2>
         </b-col>
       </b-row>
-    </b-container>
-    <b-container>
       <b-row align-h="between" align-v="center">
         <b-col sm-4>
           <b-row align-h="between" align-v="center">
-            <b-col sm-4></b-col>
             <b-col sm-4>
-              <b-form-checkbox
-                size="lg"
-                v-for="player in melee"
-                v-model="selectedMelee"
-                :key="player.value"
-                :value="player.value"
-                name="melee-player"
-              >
-              {{ player.text }}
-              </b-form-checkbox>
+              <div v-for="player in melee" class="player-name" v-bind:key="player">
+                  {{ player.text }}
+              </div>
             </b-col>
-            <b-col sm-4></b-col>
           </b-row>
         </b-col>
         <b-col sm-4>
           <b-row align-h="between" align-v="center">
-            <b-col sm-4></b-col>
             <b-col sm-4>
-              <b-form-checkbox
-                size="lg"
-                v-for="player in ultimate"
-                v-model="selectedUltimate"
-                :key="player.value"
-                :value="player.value"
-                name="ultimate-player"
-              >
-              {{ player.text }}
-              </b-form-checkbox>
+              <div v-for="player in ultimate" class="player-name" v-bind:key="player">
+                  {{ player.text }}
+              </div>
             </b-col>
-            <b-col sm-4></b-col>
           </b-row>
         </b-col>
         <b-col sm-4>
+            <b-row align-h="between" align-v="center">
+            <b-col sm-4>
+              <div v-for="player in both" class="player-name" v-bind:key="player">
+                  {{ player.name_1 }}/{{ player.name_2 }}
+              </div>
+            </b-col>
+          </b-row>
         </b-col>
       </b-row>
     </b-container>
@@ -62,17 +77,18 @@
 </template>
 
 <script>
-
-// import draggable from 'vuedraggable';
+import axios from 'axios';
+import { ModelSelect } from 'vue-search-select';
 
 export default {
   name: 'ReviewPlayers',
   components: {
-    // draggable,
+    ModelSelect,
   },
   data() {
     return {
       errors: [],
+      token: this.$store.getters.getToken,
       loggedIn: this.$store.getters.isLoggedIn,
       selectedMelee: [],
       selectedUltimate: [],
@@ -85,8 +101,38 @@ export default {
         { text: 'TaylerMailr', value: 'TaylerMailr' },
       ],
       both: [
+        { name_1: 'Scoot', name_2: 'Scoot', value: 'CamBlam' },
+        { name_1: 'GreenTiger', name_2: 'GrnTgr', value: 'CamBlam' },
       ],
+      player_data: {
+        event_id: this.$store.getters.getEventID,
+        players: [
+          {
+            id_1: '',
+            id_2: '',
+          },
+        ],
+      },
     };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      const payload = {
+        player_data: this.player_data,
+      };
+      this.mergePlayers(payload);
+    },
+    mergePlayers(payload) {
+      const path = 'http://localhost:5000/event/players';
+      axios.post(path, payload, { headers: { 'x-access-token': this.token } })
+        .then(() => {
+          this.$router.push('/matches');
+        })
+        .catch((error, msg) => {
+          this.errors.push(error + msg);
+        });
+    },
   },
   computed: {
     meleeCount() {
@@ -101,15 +147,25 @@ export default {
 
 <style scoped>
 
-.custom-checkbox.b-custom-control-lg, .input-group-lg .custom-checkbox {
-  text-align: left !important;
+.player-name {
   line-height: 2;
+  font-size: 1.15em;;
 }
 
-.custom-checkbox {
-  display: flex;
-  align-items: center;
+.merge-form-submit {
+  width: 30%;
+  background-color: #0066FF !important;
+  align-content: center;
 }
+
+.btn-next {
+  background-color: #0066FF !important;
+}
+
+.review-header {
+    text-decoration: underline;
+}
+
 
 @media only screen and (max-width: 600px) {
   .form-title{
@@ -118,6 +174,14 @@ export default {
 
   #titleCard{
     padding-bottom: 1.5em;
+  }
+
+  .player-name {
+    font-size: 0.85em;
+  }
+
+  .review-header {
+    font-size: 1.5em;
   }
 }
 
