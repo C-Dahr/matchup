b8e0162220ef1cf8629bf95cecf80701cf5f4097
Zachary Ferguson <zacharyferg98@gmail.com>
2020-03-30 21:52:55 -0300

    Fixed the title of match queue screen to properly display event name.

--- client/src/views/MatchQueue.vue
+++ client/src/views/MatchQueue.vue
@@ -5,13 +5,15 @@
         <b-col sm="2"/>
         <b-col sm="8">
           <div class="form-title">
-            <p>Event Name
+            <p>{{eventName}}
               <a v-if=loggedIn href="editEvent"><font-awesome-icon icon="cog"/></a>
             </p>
           </div>
         </b-col>
         <b-col sm="2">
-          <input class="btn btn-danger" v-if=loggedIn v-on:click="endEvent()" value="End Event"/>
+          <button class="btn btn-danger " v-if=loggedIn v-on:click="endEvent()">
+            End Event
+          </button>
         </b-col>
       </b-row>
     </b-container>
@@ -42,6 +44,7 @@ export default {
       loggedIn: this.$store.getters.isLoggedIn,
       token: this.$store.getters.getToken,
       eventID: this.$store.getters.getEventID,
+      eventName: this.$store.getters.getEventName,
       matchData: [],
     };
   },
